95152fb02344987788f65b6363230b6ab5e02292
Manikandan Kandasamy <manikandank@sycamoreinformatics.com>
2016-01-19 23:18:46 +0530

    Lockable Generator added

---
+++ lib/generators/monban/lockable/lockable_generator.rb
@@ -0,0 +1,26 @@
+require 'rails/generators/active_record'
+
+module Monban
+  module Generators
+    class LockableGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      source_root File.expand_path("../../templates", __FILE__)
+
+      def self.next_migration_number(dir)
+        ActiveRecord::Generators::Base.next_migration_number(dir)
+      end
+
+      def add_model
+        migration_template "db/migrate/add_lockable_fields_to_users.rb", "db/migrate/add_lockable_fields_to_users.rb"
+      end
+
+      def add_translations
+        template "config/locales/monban.en.yml"
+      end
+
+      def display_readme
+        readme 'lockable_readme'
+      end
+    end
+  end
+end
